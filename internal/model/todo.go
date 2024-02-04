6d26a3e31f9952544ecab0d13683a47c94629e88
victor <v7ktory@gmail.com>
2024-02-04 14:53:10 +0700

    deleted tags

--- internal/model/todo.go
+++ internal/model/todo.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Todo struct {
-	ID          int32     `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Completed   bool      `json:"completed"`
-	CreatedAt   time.Time `json:"created_at"`
-	UserID      int32     `json:"user_id"`
+	ID          int32
+	Title       string
+	Description string
+	Completed   bool
+	CreatedAt   time.Time
+	UserID      int32
 }
 type TodoReq struct {
 	Title       string `form:"title"`
--- internal/model/user.go
+++ internal/model/user.go
@@ -1,20 +1,20 @@
 package model
 
 type User struct {
-	ID       int32  `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID       int32
+	Name     string
+	Email    string
+	Password string
 }
 type UserInfo struct {
-	Sex      string `json:"sex"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Birthday string `json:"birthday"`
+	Sex      string
+	Name     string
+	Email    string
+	Birthday string
 }
 
 type UserSession struct {
-	ID    int32  `form:"id"`
-	Name  string `form:"name"`
-	Email string `form:"email"`
+	ID    int32
+	Name  string
+	Email string
 }
